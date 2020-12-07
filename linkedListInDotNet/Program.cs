860c601ce78e124fee5649b98687a787a07e2462
“teji1998” <tejasvinirpk@gmail.com>
2020-12-07 22:47:11 +0530

    Added:Welcome message

--- linkedListInDotNet/Program.cs
+++ linkedListInDotNet/Program.cs
@@ -6,7 +6,7 @@ namespace linkedListInDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the world of linkedlist !!!!");
         }
     }
 }

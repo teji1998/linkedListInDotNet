02b063a13a7c0814db258b1de42e21165eae631e
“teji1998” <tejasvinirpk@gmail.com>
2020-12-08 09:35:01 +0530

    Added:Test cases for adding and appending data

--- linkedListTest/UnitTest1.cs
+++ linkedListTest/UnitTest1.cs
@@ -15,10 +15,39 @@ namespace linkedListTest
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void givenThreeNumbers_WhenAddedToLinkedList_ShouldHaveLastNumberAddedAtFront()
         {
-            
+            int firstNode = 70;
+            int secndNode = 30;
+            int thirdNode = 56;
 
+            linkedList.AddData(70);
+            linkedList.AddData(30);
+            linkedList.AddData(56);
+            int nodeOne = linkedList.CheckingFirstElement();
+            int nodeTwo = linkedList.CheckingMiddleElement();
+            int nodeThree = linkedList.CheckingLastElement();
+            bool result = nodeOne.Equals(thirdNode) && nodeTwo.Equals(secndNode) && nodeThree.Equals(firstNode);
+            Assert.IsTrue(result);
+        }
+
+        
+        [TestMethod]
+        public void givenThreeNumbers_WhenAppendedToLinkedList_ShouldBeAddedAtLast()
+        {
+            int nodeOne = 56;
+            int nodeTwo = 30;
+            int nodeThree = 70;
+
+            linkedList.AppendData(nodeOne);
+            linkedList.AppendData(nodeTwo);
+            linkedList.AppendData(nodeThree);
+            int firstNode = linkedList.CheckingFirstElement();
+            int secondNode = linkedList.CheckingMiddleElement();
+            int thirdNode = linkedList.CheckingLastElement();
+
+            bool result = firstNode.Equals(nodeOne) && secondNode.Equals(nodeTwo) && thirdNode.Equals(nodeThree);
+            Assert.IsTrue(result);
         }
     }
 }
